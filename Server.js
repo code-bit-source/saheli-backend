e53888324907578fa21344ea81769e663d2dbf04
Amrit Kumar <amritmr760@gmail.com>
2025-12-08 15:48:30 +0530

    small 1

--- Server.js
+++ Server.js
@@ -18,13 +18,13 @@ const app = express();
 // Security + Performance Middleware
 // -------------------------
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 app.use(
   helmet({
